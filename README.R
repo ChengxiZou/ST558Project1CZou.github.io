9138dba55bc4e1114b5ee2a77f6984fdfdccc917
ChengxiZou <czou2@ncsu.edu>
2022-06-25 14:58:18 -0400

    initial settings.

---
+++ README.R
@@ -0,0 +1,4 @@
+render("ST558P1CZ.rmd",output_format = "github_document",
+        output_dir = "D:/5th semester/ST558/Rrepo/ST558Project1CZ.github.io",
+        output_options = list(html_preview = FALSE),
+        output_file = "README.md")
