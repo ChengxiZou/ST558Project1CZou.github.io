74918012919b0c6b51eedaa160eca973b9b7b520
ChengxiZou <czou2@ncsu.edu>
2022-06-25 22:56:45 -0400

    function to get berry data.

--- README.R
+++ README.R
@@ -1,4 +1,6 @@
+library(rmarkdown)
 render("ST558P1CZ.rmd",output_format = "github_document",
-        output_dir = "D:/5th semester/ST558/Rrepo/ST558Project1CZ.github.io",
+        output_dir = "D:/5th semester/ST558/Rrepo/ST558Project1CZou.github.io",
         output_options = list(html_preview = FALSE),
         output_file = "README.md")
+
